ff8218dce8f0a6182c0a27094045ef3d52dfa147
PHONGPC\minhp <minhphonghp2003@gmail.com>
2025-11-07 22:48:39 +0700

    ..

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -28,6 +28,7 @@ export default function AdminLayout({
   }
   useEffect(() => {
     dispatch(setUser(getAuthInfo().user))
+    fetchMenus()
   }, [])
 
 
@@ -40,19 +41,19 @@ export default function AdminLayout({
 
   return (
 
-      <div className="min-h-screen xl:flex">
-        {/* Sidebar and Backdrop */}
-        <AppSidebar />
-        <Backdrop />
-        {/* Main Content Area */}
-        <div
-          className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
-        >
-          {/* Header */}
-          <AppHeader />
-          {/* Page Content */}
-          <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
-        </div>
+    <div className="min-h-screen xl:flex">
+      {/* Sidebar and Backdrop */}
+      <AppSidebar />
+      <Backdrop />
+      {/* Main Content Area */}
+      <div
+        className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
+      >
+        {/* Header */}
+        <AppHeader />
+        {/* Page Content */}
+        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
       </div>
+    </div>
   );
 }
