595e7f4efac67bc02fde5a149d1c5671e5beec28
PHONGPC\minhp <minhphonghp2003@gmail.com>
2025-11-10 07:38:12 +0700

    ...

--- src/app/(admin)/(RBAC)/menu/menu-items/add-menu-modal.tsx
+++ src/app/(admin)/(RBAC)/menu/menu-items/add-menu-modal.tsx
@@ -1,5 +1,12 @@
+import DefaultInputs from '@/components/form/form-elements/DefaultInputs';
+import DropzoneComponent from '@/components/form/form-elements/DropZone';
+import Input from '@/components/form/input/InputField';
+import Label from '@/components/form/Label';
+import Select from '@/components/form/Select';
 import ActionModal from '@/components/ui/modal/ActionModal'
+import { ChevronDownIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
+import Dropzone from 'react-dropzone';
 
 function AddMenuModal({ isOpen, closeModal }: { isOpen: boolean, closeModal: any }) {
     const handleAddMenu = () => {
@@ -12,9 +19,16 @@ function AddMenuModal({ isOpen, closeModal }: { isOpen: boolean, closeModal: any
         }
 
     }, [isOpen])
+    // string Name, string Path,int GroupId, string? Description, string? Icon, int? ParentId
     return (
-        <ActionModal isOpen={isOpen} closeModal={closeModal} onConfirm={handleAddMenu} heading={"Thêm mới menu"} >
-            hihi
+        <ActionModal size="md" isOpen={isOpen} closeModal={closeModal} onConfirm={handleAddMenu} heading={"Thêm mới menu"} >
+            <div>
+                <Label>Input</Label>
+                <Input type="text" />
+                <Label>Path</Label>
+                <Input type="text" />
+               
+            </div>
         </ActionModal>
     )
 }
