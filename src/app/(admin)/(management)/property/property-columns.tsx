8bf45038491df19ca9b2b733fe87cfeb136f40a2
Minh Phong <minhphonghp2003@gmail.com>
2026-02-23 18:20:36 +0700

    ...

--- src/app/(admin)/(management)/property/property-columns.tsx
+++ src/app/(admin)/(management)/property/property-columns.tsx
@@ -12,9 +12,6 @@ const formatter = new Intl.DateTimeFormat('en-GB', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    hour12: false
 });
 
 export interface PropertyColumnProp {
